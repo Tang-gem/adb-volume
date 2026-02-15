a01f4618869640efaa19ba314b4d1e4f3f06d349
Tang-gem <2940402235@qq.com>
2026-02-15 18:35:07 +0800

    Refactor button creation for volume control
    Refactored button creation for volume control.

--- 改变音量.c
+++ 改变音量.c
@@ -1,41 +1,34 @@
 #include <windows.h>
 #include <stdlib.h>
 #include <string.h>
-
 #define ID_BTN_UP     1
 #define ID_BTN_DOWN   2
 #define ADB_PATH      "D:\\scrcpy-win64-v3.3.4\\adb.exe"
 #define CMD_UP        "shell input keyevent 24"
 #define CMD_DOWN      "shell input keyevent 25"
 #define LIMIT_INTERVAL 30
-
 DWORD g_last_adb_call_time = 0;
-
 void run_adb_command(int up) {
     DWORD current_time = GetTickCount();
     if (current_time - g_last_adb_call_time < LIMIT_INTERVAL) {
         return;
     }
     g_last_adb_call_time = current_time;
-
     STARTUPINFO si = {0};
     PROCESS_INFORMATION pi = {0};
     char cmdLine[MAX_PATH] = {0};
     strncpy(cmdLine, ADB_PATH, MAX_PATH - 1);
     strncat(cmdLine, " ", MAX_PATH - strlen(cmdLine) - 1);
     strncat(cmdLine, up ? CMD_UP : CMD_DOWN, MAX_PATH - strlen(cmdLine) - 1);
-
     si.cb = sizeof(STARTUPINFO);
     si.dwFlags = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
     si.wShowWindow = SW_HIDE;
-
     HANDLE hNull = CreateFile("NUL", GENERIC_WRITE, FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     if (hNull == INVALID_HANDLE_VALUE) {
         return;
     }
     si.hStdError = hNull;
     si.hStdOutput = hNull;
-
     BOOL bRet = CreateProcess(
         NULL,
         cmdLine,
@@ -48,14 +41,12 @@ void run_adb_command(int up) {
         &si,
         &pi
     );
-
     if (bRet) {
         if (pi.hProcess) CloseHandle(pi.hProcess);
         if (pi.hThread) CloseHandle(pi.hThread);
     }
     if (hNull != INVALID_HANDLE_VALUE) CloseHandle(hNull);
 }
-
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch(msg) {
         case WM_COMMAND: {
@@ -84,7 +75,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     }
     return 0;
 }
-
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     WNDCLASS wc = {0};
     wc.lpfnWndProc = WndProc;
@@ -92,16 +82,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wc.lpszClassName = "AdbVolumeCtrl";
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
     wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-
     if (!RegisterClass(&wc)) {
         MessageBox(NULL, "窗口类注册失败！", "错误", MB_ICONERROR | MB_OK);
         return 1;
     }
-
     // 窗口尺寸
     int win_w = 250;
     int win_h = 120;
-
     // 获取屏幕尺寸（包含任务栏）
     int screen_w = GetSystemMetrics(SM_CXSCREEN);
     int screen_h = GetSystemMetrics(SM_CYSCREEN);
@@ -110,7 +97,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     // 额外减20是为了避开任务栏，避免窗口被遮挡
     int x = screen_w - win_w - 300;
     int y = screen_h - win_h - 50;
-
     HWND hwnd = CreateWindowEx(
         0,
         "AdbVolumeCtrl",
@@ -119,25 +105,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         x, y, win_w, win_h, // 使用右下角坐标
         NULL, NULL, hInstance, NULL
     );
-
     if (!hwnd) {
         MessageBox(NULL, "窗口创建失败！", "错误", MB_ICONERROR | MB_OK);
         return 1;
     }
-
-    CreateWindow("BUTTON", "Volume +",
-        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
-        30, 30, 80, 40,
-        hwnd, (HMENU)ID_BTN_UP, hInstance, NULL);
-
+    // 左侧按钮：减音量（修改点1）
     CreateWindow("BUTTON", "Volume -",
         WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
-        130, 30, 80, 40,
+        30, 30, 80, 40,
         hwnd, (HMENU)ID_BTN_DOWN, hInstance, NULL);
-
+    // 右侧按钮：加音量（修改点2）
+    CreateWindow("BUTTON", "Volume +",
+        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+        130, 30, 80, 40,
+        hwnd, (HMENU)ID_BTN_UP, hInstance, NULL);
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
-
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
